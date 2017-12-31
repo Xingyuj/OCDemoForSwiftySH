92ab3bc32a3d0acfabb344745f1aa2a9ad2fa5c8
EthanXingyuJi <endorathas@gmail.com>
2017-12-31 14:29:10 +1100

    submit OC demo

--- OCDemoForSwiftySH/AppDelegate.m
+++ OCDemoForSwiftySH/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <SHSdkSwift/SHSdkSwift-Swift.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [SHClientsManager setupWithAppKey: @"hipointX"];
     return YES;
 }
 
--- OCDemoForSwiftySH/ViewController.m
+++ OCDemoForSwiftySH/ViewController.m
@@ -7,12 +7,24 @@
 //
 
 #import "ViewController.h"
+#import <SHSdkSwift/SHSdkSwift-Swift.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *tagKey;
+@property (weak, nonatomic) IBOutlet UITextField *tagValue;
 @end
 
 @implementation ViewController
+
+- (IBAction)logTag:(id)sender {
+    NSDictionary *content = [NSDictionary dictionaryWithObjectsAndKeys:[self.tagKey text], @"key", [self.tagValue text], @"string", nil];
+    [SHClientsManager.shProcessor tagViaLogline:content];
+}
+
+- (IBAction)apiTag:(id)sender {
+    NSDictionary *content = [NSDictionary dictionaryWithObjectsAndKeys:[self.tagKey text], @"key", [self.tagValue text], @"string", nil];
+    [SHClientsManager.shProcessor tagViaApi:content authToken: @"1JMyBIGTLLA86MxJ7nCm7kBZoSiOmJ"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,5 +37,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing: YES];
+}
 
 @end
