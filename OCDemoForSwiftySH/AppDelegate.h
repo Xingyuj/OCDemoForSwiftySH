589e30b47ec98fcc71f8855143d8c01f05f112cf
EthanXingyuJi <endorathas@gmail.com>
2017-12-31 11:11:08 +1100

    Initial Commit

---
+++ OCDemoForSwiftySH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCDemoForSwiftySH
+//
+//  Created by Xingyuji on 31/12/17.
+//  Copyright © 2017 com.xingyuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCDemoForSwiftySH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCDemoForSwiftySH
+//
+//  Created by Xingyuji on 31/12/17.
+//  Copyright © 2017 com.xingyuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
